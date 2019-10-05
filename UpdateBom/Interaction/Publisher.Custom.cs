fc056e8d710cc8bab8810fd0c9d6743c6b39ba71
Andrew Akenson <andrew.akenson@autodesk.com>
2019-10-04 17:27:33 -0700

    Extracts BOM from assembly

---
+++ UpdateBom/Interaction/Publisher.Custom.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using Autodesk.Forge.DesignAutomation.Model;
+
+namespace Interaction
+{
+    /// <summary>
+    /// Customizable part of Publisher class.
+    /// </summary>
+    internal partial class Publisher
+    {
+        /// <summary>
+        /// Constants.
+        /// </summary>
+        private static class Constants
+        {
+            private const int EngineVersion = 24;
+            public static readonly string Engine = $"Autodesk.Inventor+{EngineVersion}";
+
+            public const string Description = "Outputs a BOM json file";
+
+            internal static class Bundle
+            {
+                public static readonly string Id = "UpdateBom";
+                public const string Label = "alpha";
+
+                public static readonly AppBundle Definition = new AppBundle
+                {
+                    Engine = Engine,
+                    Id = Id,
+                    Description = Description
+                };
+            }
+
+            internal static class Activity
+            {
+                public static readonly string Id = Bundle.Id;
+                public const string Label = Bundle.Label;
+            }
+
+            internal static class Parameters
+            {
+                public const string inputFile = nameof(inputFile);
+                public const string inputParams = nameof(inputParams);
+                public const string outputFile = nameof(outputFile);
+            }
+        }
+
+
+        /// <summary>
+        /// Get command line for activity.
+        /// </summary>
+        private static List<string> GetActivityCommandLine()
+        {
+            return new List<string> { $"$(engine.path)\\InventorCoreConsole.exe /al $(appbundles[{Constants.Activity.Id}].path)" };
+        }
+
+        /// <summary>
+        /// Get activity parameters.
+        /// </summary>
+        private static Dictionary<string, Parameter> GetActivityParams()
+        {
+            return new Dictionary<string, Parameter>
+                    {
+                         {
+                            Constants.Parameters.inputFile,
+                            new Parameter
+                            {
+                                Verb = Verb.Get,
+                                Description = "Input assembly to extract parameters",
+                                LocalName = "inputFile",
+                                Zip = true
+                            }
+                        },
+                        {
+                            Constants.Parameters.inputParams,
+                            new Parameter
+                            {
+                                Verb = Verb.Get,
+                                Description = "Input json file tells what to load and from where",
+                                LocalName = "inputParams.json"
+                            }
+                        },
+                       {
+                            Constants.Parameters.outputFile,
+                            new Parameter
+                            {
+                                Verb = Verb.Put,
+                                LocalName = "bomRows.json",
+                                Description = "Resulting BOM Rows data",
+                            }
+                        }
+                    };
+        }
+
+        /// <summary>
+        /// Get arguments for workitem.
+        /// </summary>
+        private static Dictionary<string, IArgument> GetWorkItemArgs()
+        {
+            // TODO: update the URLs below with real values
+            return new Dictionary<string, IArgument>
+                    {
+                    //    {
+                    //        Constants.Parameters.InventorDoc,
+                    //        new XrefTreeArgument
+                    //        {
+                    //            Url = "!!! CHANGE ME !!!"
+                    //        }
+                    //    },
+                    //    {
+                    //        Constants.Parameters.OutputIpt,
+                    //        new XrefTreeArgument
+                    //        {
+                    //            Verb = Verb.Put,
+                    //            Url = "!!! CHANGE ME !!!"
+                    //        }
+                    //    }
+                    };
+        }
+    }
+}
---
+++ UpdateBom/UpdateBomPlugin/SampleAutomation.cs
@@ -0,0 +1,175 @@
+﻿/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+using Inventor;
+using Autodesk.Forge.DesignAutomation.Inventor.Utils;
+
+using Newtonsoft.Json;
+
+using File = System.IO.File;
+using Path = System.IO.Path;
+using Directory = System.IO.Directory;
+using Newtonsoft.Json.Linq;
+
+namespace UpdateBomPlugin
+{
+    [ComVisible(true)]
+    public class SampleAutomation
+    {
+        private readonly InventorServer inventorApplication;
+
+        public SampleAutomation(InventorServer inventorApp)
+        {
+            inventorApplication = inventorApp;
+        }
+
+        public void Run(Document placeholder)
+        {
+            // LogTrace("Run called with {0}", doc.DisplayName);
+
+            LogTrace("Running Update BOM...");
+            string currDir = Directory.GetCurrentDirectory();
+
+            LogTrace("currDir: " + currDir);
+            // For local debugging
+            //string inputPath = System.IO.Path.Combine(currDir, @"../../inputFiles", "params.json");
+            //Dictionary<string, string> options = JsonConvert.DeserializeObject<Dictionary<string, string>>(System.IO.File.ReadAllText(inputPath));
+
+            Dictionary<string, string> options = JsonConvert.DeserializeObject<Dictionary<string, string>>(System.IO.File.ReadAllText("inputParams.json"));
+            string inputFile = options["inputFile"];
+            string projectFile = options["projectFile"];
+
+            string assemblyPath = Path.GetFullPath(Path.Combine(currDir, inputFile));
+
+            string fullProjectPath = Path.GetFullPath(Path.Combine(currDir, projectFile));
+
+            Console.WriteLine("fullProjectPath = " + fullProjectPath);
+
+            DesignProject dp = inventorApplication.DesignProjectManager.DesignProjects.AddExisting(fullProjectPath);
+            dp.Activate();
+
+            Console.WriteLine("assemblyPath = " + assemblyPath);
+            Document doc = inventorApplication.Documents.Open(assemblyPath);
+
+            using (new HeartBeat())
+            {
+                GetBom(doc);
+            }
+        }
+
+        public void RunWithArguments(Document doc, NameValueMap map)
+        {
+            LogTrace("RunWithArguments not implemented");
+        }
+
+        public void GetBom(Document doc)
+        {
+            try
+            {
+                AssemblyDocument assemblyDoc = doc as AssemblyDocument;
+                AssemblyComponentDefinition componentDef = assemblyDoc.ComponentDefinition;
+                BOM bom = componentDef.BOM;
+                BOMViews bomViews = bom.BOMViews;
+                BOMView structureView = bomViews["Structured"];
+
+                JArray bomRows = new JArray();
+                GetBomRowProperties(structureView.BOMRows, bomRows);
+
+                LogTrace("Writing out bomRows.json");
+                File.WriteAllText("bomRows.json", bomRows.ToString());
+
+            }
+            catch (Exception e)
+            {
+                LogError("Bom failed: " + e.ToString());
+            }
+        }
+
+        public void GetBomRowProperties(BOMRowsEnumerator rows, JArray bomRows)
+        {
+            const string TRACKING = "Design Tracking Properties";
+            foreach (BOMRow row in rows)
+            {
+                ComponentDefinition componentDef = row.ComponentDefinitions[1];
+
+                // Assumes not virtual component (if so add conditional for that here)
+                Property partNum = componentDef.Document.PropertySets[TRACKING]["Part Number"];
+                Property descr = componentDef.Document.PropertySets[TRACKING]["Description"];
+                Property material = componentDef.Document.PropertySets[TRACKING]["Material"];
+
+                JObject bomRow = new JObject(
+                    new JProperty("row_number", row.ItemNumber),
+                    new JProperty("part_number", partNum.Value),
+                    new JProperty("quantity", row.ItemQuantity),
+                    new JProperty("description", descr.Value),
+                    new JProperty("material", material.Value)
+                    );
+
+                bomRows.Add(bomRow);
+
+                // iterate through child rows
+                if (row.ChildRows != null)
+                {
+                    GetBomRowProperties(row.ChildRows, bomRows);
+                }
+            }
+        }
+
+
+        #region Logging utilities
+
+        /// <summary>
+        /// Log message with 'trace' log level.
+        /// </summary>
+        private static void LogTrace(string format, params object[] args)
+        {
+            Trace.TraceInformation(format, args);
+        }
+
+        /// <summary>
+        /// Log message with 'trace' log level.
+        /// </summary>
+        private static void LogTrace(string message)
+        {
+            Trace.TraceInformation(message);
+        }
+
+        /// <summary>
+        /// Log message with 'error' log level.
+        /// </summary>
+        private static void LogError(string format, params object[] args)
+        {
+            Trace.TraceError(format, args);
+        }
+
+        /// <summary>
+        /// Log message with 'error' log level.
+        /// </summary>
+        private static void LogError(string message)
+        {
+            Trace.TraceError(message);
+        }
+
+        #endregion
+    }
+}
